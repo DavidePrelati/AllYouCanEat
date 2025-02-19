7eca22069ac90367de887d0cb439d3c46319bad7
DavidePrelati <davide.prelati@gmail.com>
2025-02-19 09:41:43 +0100

    update4

--- index.js
+++ index.js
@@ -46,7 +46,43 @@ function caricaFile(fileName) {
     })
 }
 
+document.getElementById('bevande').addEventListener('click', function (event){
+    event.preventDefault()
+    caricaFile("bevande.html");
+})
+
+document.getElementById('antipasti').addEventListener('click', function (event){
+    event.preventDefault()
+    caricaFile("antipasti.html");
+})
+
+document.getElementById('antipasti_crudi').addEventListener('click', function (event){
+    event.preventDefault()
+    caricaFile("antipasti_crudi.html");
+})
+
 document.getElementById("nigiri").addEventListener('click', function (event) {
     event.preventDefault()
      caricaFile("nigiri.html")
+})
+
+
+document.getElementById('hosomaki').addEventListener('click', function (event){
+    event.preventDefault()
+    caricaFile("hosomaki.html");
+})
+
+document.getElementById('uramaki').addEventListener('click', function (event){
+    event.preventDefault()
+    caricaFile("uramaki.html");
+})
+
+document.getElementById('special').addEventListener('click', function (event){
+    event.preventDefault()
+    caricaFile("special.html");
+})
+
+document.getElementById('dolci').addEventListener('click', function (event){
+    event.preventDefault()
+    caricaFile("dolci.html");
 })
