645d82613552f32b7ada60134f6542f60a03713e
Nardi-Francesco <nardi.francesco30@gmail.com>
2025-03-07 00:07:26 +0100

    update27

---
+++ javascript/hosomaki.js
@@ -0,0 +1,18 @@
+document.querySelectorAll(".add-to-cart").forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+  
+      const name = this.getAttribute("data-name");
+      const price = this.getAttribute("data-price");
+      const imageSrc = this.closest(".card").querySelector("img").src;
+  
+      const product = { name, price, imageSrc };
+  
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  
+      cart.push(product);
+  
+      localStorage.setItem("cart", JSON.stringify(cart));
+  
+    });
+  });
